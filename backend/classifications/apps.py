8df9515320cdfd3624eec33f46597b99661182fa
Lathrell <qltpagsuguiron@tip.edu.ph>
2025-08-04 22:53:54 +0800

    Remove backend application files and configurations

--- backend/classifications/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClassificationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'classifications'
--- backend/classifications/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('new', views.new_case, name="new_case"),
-    path('', views.index, name="index")
-]
