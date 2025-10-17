835922dbd60f9a3804035b9e55813d0bc6c678c0
Richard William <rwilliam.flores@gmail.com>
2025-10-17 16:13:27 +0800

    Refactor: Adjust padding in ChatContainer for improved layout

--- frontend/components/patient/diagnosis-page/chat-container.tsx
+++ frontend/components/patient/diagnosis-page/chat-container.tsx
@@ -7,7 +7,7 @@ type ChatContainerProps = {
 
 const ChatContainer = ({ messages }: ChatContainerProps) => {
   return (
-    <section className="flex flex-col flex-1 space-y-2 px-52 py-8">
+    <section className="flex flex-col flex-1 space-y-2 px-44 py-8">
       {messages.map((message) => (
         <ChatBubble key={message.id + message.content} {...message} />
       ))}
