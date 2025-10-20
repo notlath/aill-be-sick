5a1c9cf188bf8e66c2f3299f71296c60aa2e00c1
Richard William <rwilliam.flores@gmail.com>
2025-10-20 15:49:53 +0800

    Refactor: Import necessary functions for path revalidation and redirection in signOutClient

--- frontend/utils/auth.ts
+++ frontend/utils/auth.ts
@@ -1,4 +1,6 @@
 import { createClient } from "./supabase/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const signOutClient = async () => {
   const supabase = await createClient();
