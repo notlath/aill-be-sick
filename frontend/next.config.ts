d83314711059644b892943584b8a39f580914c2d
Richard William <rwilliam.flores@gmail.com>
2026-02-27 20:22:29 +0800

    refactor: remove PrismaPlugin from webpack config and add proxy functionality

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,7 @@
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-  webpack: (config: any, { isServer }: { isServer: boolean }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-    return config;
-  },
   images: {
     remotePatterns: [
       {
